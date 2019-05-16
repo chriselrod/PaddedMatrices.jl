f20d45689004293739e0d5bab2431f9735e43d00
Chris Elrod <elrodc@gmail.com>
2019-05-16 16:25:36 -0500

    Fixed bug in maximum(::typeof(abs),::FixedSizePaddedArray)

--- src/utilities.jl
+++ src/utilities.jl
@@ -37,28 +37,110 @@ end
 Base.cumsum(A::AbstractMutableFixedSizePaddedVector) = cumsum!(copy(A))
 Base.cumsum(A::AbstractConstantFixedSizePaddedVector) = ConstantFixedSizePaddedVector(cumsum!(MutableFixedSizePaddedVector(A)))
 
-@generated function Base.maximum(A::AbstractFixedSizePaddedArray{S,T,P,L}) where {S,T,P,L}
-    W, Wshift = VectorizationBase.pick_vector_width_shift(L, T)
-    quote
-        m = SIMDPirates.vbroadcast(Vec{$W, $T}, -Inf)
-        @vectorize $T for l ∈ 1:$L
-            a = A[l]
-            m = SIMDPirates.vmax(a, m)
-        end
-        SIMDPirates.maximum(m)
+# @generated function Base.maximum(A::AbstractFixedSizePaddedArray{S,T,P,L}) where {S,T,P,L}
+#     W, Wshift = VectorizationBase.pick_vector_width_shift(L, T)
+#     V = Vec{W,T}
+#     q = quote
+#         $(Expr(:meta,:inline))
+#         m = SIMDPirates.vbroadcast($V, -Inf)
+#         pA = VectorizationBase.vectorizable(A)
+#     end
+#     fulliters = L >> Wshift
+#     if fulliters > 0
+#         push!(q.args,
+#         quote
+#             for l ∈ 0:$(fulliters-1)
+#                 a = SIMDPirates.vabs(SIMDPirates.vload($V, pA + l*$W))
+#                 m = SIMDPirates.vmax(a, m)
+#             end
+#         end)
+#     end
+#     r = L & (W - 1)
+#     if r > 0
+#         U = VectorizationBase.mask_type(W)
+#         push!(q.args, quote
+#             mask = $( U(2^r-1) )
+#             a = SIMDPirates.vabs(SIMDPirates.vload($V, pA + $(fulliters*W), mask ))
+#             m = vmax(m, a)
+#         end)
+#     end
+#     push!(q.args, :(SIMDPirates.vmaximum(m)))
+#     q
+# end
+
+# @generated
+function Base.maximum(::typeof(abs), A::AbstractFixedSizePaddedArray{S,T,P,L}) where {S,T,P,L}
+    SV = S.parameters
+    R1 = SV[1]
+    D = 1
+    for i ∈ 2:length(SV)
+        D *= SV[i]
     end
+    W, Wshift = VectorizationBase.pick_vector_width_shift(R1, T)
+    V = Vec{W,T}
+    q = quote
+        $(Expr(:meta,:inline))
+        m = SIMDPirates.vbroadcast($V, -Inf)
+        pA = VectorizationBase.vectorizable(A)
+    end
+    fulliters = R1 >> Wshift
+    innerloop = quote end
+    if fulliters > 0
+        push!(innerloop.args,
+        quote
+            for l ∈ 0:$(fulliters-1)
+                a = SIMDPirates.vabs(SIMDPirates.vload($V, pA + l*$W + j*$(P*W) ))
+                m = SIMDPirates.vmax(a, m)
+            end
+        end)
+    end
+    r = L & (W - 1)
+    if r > 0
+        U = VectorizationBase.mask_type(W)
+        push!(innerloop.args, quote
+            mask = $( U(2^r-1) )
+            a = SIMDPirates.vabs(SIMDPirates.vload($V, pA + $(fulliters*W), mask ))
+            m = SIMDPirates.vmax(m, a)
+        end)
+    end
+    push!(q.args, quote
+        for j ∈ 0:$(D-1)
+            $innerloop
+        end
+        SIMDPirates.vmaximum(m)
+    end)
+    q
 end
 
-@generated function Base.maximum(::typeof(abs), A::AbstractFixedSizePaddedArray{S,T,P,L}) where {S,T,P,L}
-    W, Wshift = VectorizationBase.pick_vector_width_shift(L, T)
-    quote
-        m = SIMDPirates.vbroadcast(Vec{$W, $T}, -Inf)
-        @vectorize $T for l ∈ 1:$L
-            a = SIMDPirates.vabs(A[l])
-            m = SIMDPirates.vmax(a, m)
-        end
-        SIMDPirates.maximum(m)
+@generated function Base.maximum(::typeof(abs), A::AbstractFixedSizePaddedVector{S,T,P,L}) where {S,T,P,L}
+    W, Wshift = VectorizationBase.pick_vector_width_shift(S, T)
+    V = Vec{W,T}
+    q = quote
+        $(Expr(:meta,:inline))
+        m = SIMDPirates.vbroadcast($V, -Inf)
+        pA = VectorizationBase.vectorizable(A)
     end
+    fulliters = S >> Wshift
+    if fulliters > 0
+        push!(q.args,
+        quote
+            for l ∈ 0:$(fulliters-1)
+                a = SIMDPirates.vabs(SIMDPirates.vload($V, pA + l*$W))
+                m = SIMDPirates.vmax(a, m)
+            end
+        end)
+    end
+    r = S & (W - 1)
+    if r > 0
+        U = VectorizationBase.mask_type(W)
+        push!(q.args, quote
+            mask = $( U(2^r-1) )
+            a = SIMDPirates.vabs(SIMDPirates.vload($V, pA + $(fulliters*W), mask ))
+            m = SIMDPirates.vmax(m, a)
+        end)
+    end
+    push!(q.args, :(SIMDPirates.vmaximum(m)))
+    q
 end
 
 @inline Base.pointer(x::Symmetric{T,MutableFixedSizePaddedMatrix{P,P,T,R,L}}) where {P,T,R,L} = pointer(x.data)
