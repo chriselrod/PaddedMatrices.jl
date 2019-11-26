c0aff947365cc53354b957c25f441e2def12fd6e
Chris Elrod <elrodc@gmail.com>
2019-11-25 20:09:18 -0500

    Added zeroinitialized for more conveniently optimizing some code.

---
+++ src/zeroinitialized.jl
@@ -0,0 +1,13 @@
+
+
+struct ZeroInitializedArray{S,T,N,X,L} <: AbstractMutableFixedSizeArray{S,T,N,X,L}
+    ptr::Ptr{T}
+end
+const ZeroInitializedVector{P,T,L} = ZeroInitializedArray{Tuple{P},T,1,Tuple{1},L}
+const ZeroInitializedMatrix{M,N,T,P,L} = ZeroInitializedArray{Tuple{M,N},T,2,Tuple{1,P},L}
+@inline Base.pointer(A::ZeroInitializedArray) = A.ptr
+@inline VectorizationBase.vectorizable(A::ZeroInitializedArray) = VectorizationBase.ZeroInitializedPointer(A.ptr)
+@inline ZeroInitializedArray(A::AbstractMutableFixedSizeArray{S,T,N,X,L}) where {S,T,N,X,L} = ZeroInitializedArray{S,T,N,X,L}(pointer(A))
+@inline VectorizationBase.zeroinitialized(A::AbstractMutableFixedSizeArray{S,T,N,X,L}) where {S,T,N,X,L} = ZeroInitializedArray{S,T,N,X,L}(pointer(A))
+
+
