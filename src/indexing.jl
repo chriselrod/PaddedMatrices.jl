81146ecae1ce6b60fff7319f13d058e2d330ece8
chriselrod <elrodc@gmail.com>
2021-01-06 05:28:56 -0500

    Fix linear indexing allocations

--- src/indexing.jl
+++ src/indexing.jl
@@ -21,9 +21,12 @@ Base.@propagate_inbounds Base.getindex(A::AbstractStrideVector, i::Int, j::Int) 
     vload(stridedpointer(A), i)
 end
 @inline function Base.getindex(A::AbstractStrideArray{S,D,T,K}, i::Vararg{Integer,K}) where {S,D,T,K}
-    @boundscheck checkbounds(A, i...)
     b = preserve_buffer(A)
-    GC.@preserve b vload(stridedpointer(A), i)
+    P = PtrArray(A)
+    GC.@preserve b begin
+        @boundscheck checkbounds(P, i...)
+        vload(stridedpointer(P), i)
+    end
 end
 @inline function Base.setindex!(A::PtrArray{S,D,T,K}, v, i::Vararg{Integer,K}) where {S,D,T,K}
     @boundscheck checkbounds(A, i...)
@@ -31,9 +34,12 @@ end
     v
 end
 @inline function Base.setindex!(A::AbstractStrideArray{S,D,T,K}, v, i::Vararg{Integer,K}) where {S,D,T,K}
-    @boundscheck checkbounds(A, i...)
     b = preserve_buffer(A)
-    GC.@preserve b vstore!(stridedpointer(A), v, i)
+    P = PtrArray(A)
+    GC.@preserve b begin
+        @boundscheck checkbounds(P, i...)
+        vstore!(stridedpointer(P), v, i)
+    end
     v
 end
 @inline function Base.getindex(A::PtrArray, i::Integer)
@@ -41,19 +47,25 @@ end
     vload(stridedpointer(A), (i - one(i),))
 end
 @inline function Base.getindex(A::AbstractStrideArray, i::Integer)
-    @boundscheck checkbounds(A, i)
     b = preserve_buffer(A)
-    GC.@preserve b vload(stridedpointer(A), (i - one(i),))
+    P = PtrArray(A)
+    GC.@preserve b begin
+        @boundscheck checkbounds(P, i)
+        vload(stridedpointer(P), (i - one(i),))
+    end
 end
 @inline function Base.setindex!(A::PtrArray, v, i::Integer)
-    @boundscheck checkbounds(A, i...)
+    @boundscheck checkbounds(A, i)
     vstore!(stridedpointer(A), v, (i - one(i),))
     v
 end
 @inline function Base.setindex!(A::AbstractStrideArray, v, i::Integer)
-    @boundscheck checkbounds(A, i...)
     b = preserve_buffer(A)
-    GC.@preserve b vstore!(stridedpointer(A), v, (i - one(i),))
+    P = PtrArray(A)
+    GC.@preserve b begin
+        @boundscheck checkbounds(P, i)
+        vstore!(stridedpointer(P), v, (i - one(i),))
+    end
     v
 end
 
